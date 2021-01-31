773aadd35b4bbc098ec09c881eef2838c3de1ca9
Radovan Bacovic <radovan.bacovic@gmail.com>
2021-01-31 08:56:16 +0100

    test

---
+++ robot_origin.py
@@ -0,0 +1,48 @@
+# https://leetcode.com/problems/robot-return-to-origin/
+
+import unittest
+
+
+class Solution:
+
+    def robot_origin(self, moves):
+        # x = 0
+        # y = 0
+
+        # for i in list(moves):
+        #     if i == 'L':
+        #         x -= 1
+        #     elif i == 'R':
+        #         x += 1
+        #     elif i == 'D':
+        #         y -= 1
+        #     elif i == 'U':
+        #         y += 1
+        #
+        # return x == y == 0
+        if not moves:
+            return True
+        return moves.count('L') == moves.count('R') and moves.count('U') == moves.count('D')
+
+# test
+
+# s = Solution()
+# test_cases = [98711, 12322] #, 1234, 123456789, 0, 12322]
+# for t in test_cases:
+#     print(t, s.robot_origin(t))
+#
+
+class TestInt(unittest.TestCase):
+
+    def test_integer(self):
+        s = Solution()
+
+        test_cases = ['UD', 'LL', 'RRDD', 'LDRRLRUULR']
+        test_results = [True, False, False, False]
+
+        for i, test_case in enumerate(test_cases):
+            self.assertEqual(s.robot_origin(test_case), test_results[i])
+
+
+if __name__ == '__main__':
+    unittest.main()
