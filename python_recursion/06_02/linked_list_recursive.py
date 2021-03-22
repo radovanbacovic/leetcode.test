40de9ed70fa28ee6923e38af1e71efd821e69739
radbac <radovan.bacovic@gmail.com>
2021-03-22 11:06:46 +0100

    Recursive python course

---
+++ python_recursion/06_02/linked_list_recursive.py
@@ -0,0 +1,33 @@
+"""
+Python Recursion Video Course
+Robin Andrews - https://compucademy.net/
+"""
+
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+def traverse(head):
+    # Base case
+    if head is None:
+        return
+    # recursive case
+    print(head.data)
+    traverse(head.next)
+
+
+item1 = Node('dog')
+item2 = Node('cat')
+item3 = Node('rat')
+
+item1.next = item2
+item2.next = item3
+
+
+# traverse(item1)
+
+head = Node('dog', Node('cat', Node('rat', None)))
+traverse(head)
---
+++ python_recursion/06_03/binary_tree_traversal.py
@@ -0,0 +1,64 @@
+"""
+Python Recursion Video Course
+Robin Andrews - https://compucademy.net/
+"""
+
+
+class Node(object):
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def preorder_print(root, path=""):
+    """Root->Left->Right"""
+    if root:
+        path += root.data + '-'
+        path = preorder_print(root.left, path)
+        path = preorder_print(root.right, path)
+    return path
+
+def inorder_print(root, path=""):
+    """Left->Root->Right"""
+    if root:
+        path = inorder_print(root.left, path)
+        path += root.data + '-'
+        path = inorder_print(root.right, path)
+    return path
+
+
+def postorder_print(root, path=""):
+    """Left->Right->Root"""
+    if root:
+        path = postorder_print(root.left, path)
+        path = postorder_print(root.right, path)
+        path += root.data + '-'
+    return path
+
+if __name__ == '__main__':
+    # Set up tree:
+    root = Node("F")
+    root.left = Node("D")
+    root.left.left = Node("B")
+    root.left.left.left = Node("A")
+    root.left.left.right = Node("C")
+    root.left.right = Node("E")
+    root.right = Node("I")
+    root.right.left = Node("G")
+    root.right.left.right = Node("H")
+    root.right.right = Node("J")
+
+print("Preorder:", preorder_print(root))
+print("Inorder:", inorder_print(root))
+print("Postorder", postorder_print(root))
+
+r"""
+          ______F______
+         /             \
+      __D__           __I__
+     /     \         /     \
+    B       E       G       J
+   / \               \
+  A   C               H
+"""
