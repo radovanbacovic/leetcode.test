f492aa92d168191567ac1ebd3fe393049905a2de
Radovan Bacovic <radovan.bacovic@gmail.com>
2021-04-01 22:26:15 +0200

    test

--- fb/sandboxx.py
+++ fb/sandboxx.py
@@ -1,22 +1,5 @@
-def make_all_combinations(list1, list2):
-    if not list1:
-        return []
-    if not list2:
-        return [x for x in list1]
-    return [x + y for x in list1 for y in list2]
+a = ['a', 'b', 'c']
 
-list1 = 'abc'
-list2 = 'def'
+a[len(a):] = ['d', 'e']
 
-
-# print(make_all_combinations(list1, list2))
-
-full_list = [[],[]]
-
-
-my_range = [(x, y) for x in range(len(full_list)) for y in range(1, len(full_list)) if x != y and y > x]
-print(my_range)
-for x, y in my_range:
-    print(make_all_combinations(full_list[x], full_list[y]))
-
-
+print((1, 2, 3, 4, 5, 6, 7, 8, 9)[1::3])
